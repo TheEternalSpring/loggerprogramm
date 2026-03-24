7b3b2677cf86bfbe4ba540686731e4970cd8fa2f
TheEternalSpring <sababuch@gmail.com>
2026-03-24 04:53:07 -0700

    stuff

--- src/main/java/ge/edu/sangu/Person.java
+++ src/main/java/ge/edu/sangu/Person.java
@@ -1,4 +1,22 @@
 package ge.edu.sangu;
 
 public class Person {
+    private String name;
+    private int age;
+    private String job;
+
+    public Person(String name, int age, String job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", job='" + job + '\'' +
+                '}';
+    }
 }
--- src/main/java/ge/edu/sangu/main.java
+++ src/main/java/ge/edu/sangu/main.java
@@ -5,6 +5,8 @@ public class main {
 
     private static final MyLogger logger = new MyLogger("launcher 1");
     public static void main(String[] args){
+        Person mike = new Person("john",24,"accountant");
+        System.out.println(mike.toString());
 
     }
 }
